c9c7e1993919f778374f9118266b191a4a0023a1
hihello <13111181+hihellos@users.noreply.github.com>
2020-07-24 09:58:28 -0400

    added password requiremnt prompts

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+// Prompt Choices & User Input
+var passLength = prompt("Choose a password length between 8-18. Enter desired character length below:");
+var upperLet = prompt("Do you want to include uppercase letters?");
+var lowerLet = prompt("Do you want to include lowercase letters?");
+var numbers = prompt("Do you want to include numbers?");
+var specChar = prompt("Do you want to include special characters?");
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
@@ -9,6 +16,9 @@ function writePassword() {
   passwordText.value = password;
 
 }
+
+document.getElementById("generate").onclick = function() {requestInput};
+
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
